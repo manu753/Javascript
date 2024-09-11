0c97a8af689107d73d20a722ae6fa652b3c5b4ac
manu753 <manojmanjunath753@gmail.com>
2024-09-11 13:05:14 +0530

    Update promises.js

--- promises.js
+++ promises.js
@@ -56,3 +56,15 @@ Promise.race([sleep(),study()]).then(function(){
 }).catch(function(){
   console.log("none of them is completed!");
 });
+
+//Promise.all()
+//Fulfills when all of the promises fulfill; rejects when any of the promises rejects.
+
+//Promise.allSettled()
+//Fulfills when all promises settle.
+
+//Promise.any()
+//Fulfills when any of the promises fulfills; rejects when all of the promises reject.
+
+//Promise.race()
+//Settles when any of the promises settles. In other words, fulfills when any of the promises fulfills; rejects when any of the promises rejects.
