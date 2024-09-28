0e89f5bb83605c3148f00df9634d390d8aae1c6f
manu753 <manojmanjunath753@gmail.com>
2024-09-28 12:32:01 +0530

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -1,3 +1,55 @@
+// fibonacci using constructor funcion
+
+function Fib(){
+  this.fibMomo = {};
+
+  this.printFibValue = function (n = 0){
+    
+    if(n === 1 || n === 0){
+      this.fibMomo[n] = n;
+      return n;
+    }
+    
+    if(this.fibMomo[n]){
+      return this.fibMomo[n];
+    }
+    else{
+     const res = this.printFibValue(n - 1) + this.printFibValue(n - 2)
+     this.fibMomo[n] = res;
+     return res;
+    }
+
+  }
+  
+}
+
+const x = new Fib();
+x.printFibValue(10)
+console.log(x)
+console.log(x.printFibValue(6));
+
+Output:
+
+Fib {
+  fibMomo: {
+    '0': 0,
+    '1': 1,
+    '2': 1,
+    '3': 2,
+    '4': 3,
+    '5': 5,
+    '6': 8,
+    '7': 13,
+    '8': 21,
+    '9': 34,
+    '10': 55
+  },
+  printFibValue: [Function (anonymous)]
+}
+8
+
+
+
 //fibonacci series
 
 function fibo(n){
